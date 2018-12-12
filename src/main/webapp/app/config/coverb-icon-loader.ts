526ba4fc357f4eb2801163785790956b3a454618
wilever <wilevergomez@gmail.com>
2018-12-12 15:49:29 -0300

    solve css problem: add section id to html

---
+++ src/main/webapp/app/config/coverb-icon-loader.ts
@@ -0,0 +1,55 @@
+/*
+// ../../../ -> ../../
+// footer
+
+import { faInstagram } from '../../../static/icon/brand/faInstagram';
+import { faTwitter } from '../../../static/icon/brand/faTwitter';
+
+// company
+
+import { faBullhorn } from '../../../static/icon/solid/faBullhorn';
+import { faChartLine } from '../../../static/icon/solid/faChartLine';
+import { faMagic } from '../../../static/icon/solid/faMagic';
+import { faCubes } from '../../../static/icon/solid/faCubes';
+import { faCogs } from '../../../static/icon/solid/faCogs';
+import { faServer } from '../../../static/icon/solid/faServer';
+
+import { faCloud } from '@fortawesome/free-solid-svg-icons/faCloud';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+export const loadIcons = () => {
+  library.add(
+    faSort,
+    faEye,
+    faSync,
+    faBan,
+    faTrash,
+    faArrowLeft,
+    faSave,
+    faPlus,
+    faPencilAlt,
+    faUser,
+    faTachometerAlt,
+    faHeart,
+    faList,
+    faTasks,
+    faBook,
+    faHdd,
+    faClock,
+    faSignInAlt,
+    faSignOutAlt,
+    faWrench,
+    faThList,
+    faUserPlus,
+    faAsterisk,
+    faFlag,
+    faBell,
+    faHome,
+    faRoad,
+    faCloud,
+    faTimesCircle,
+    faSearch
+  );
+};
+*/
