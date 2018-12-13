8ca19a62848a541d0e84f805648f1fe074d7a17b
wilever <wilevergomez@gmail.com>
2018-12-13 12:47:35 -0300

    add footer to demo, remove from covers

--- src/main/webapp/app/cover/default/default.tsx
+++ src/main/webapp/app/cover/default/default.tsx
@@ -91,22 +91,6 @@ export class Default extends Component {
               </Col>
             </Row>
           </div>
-          <footer className="container-fluid text-center bg-gray">
-            <Row className="text-center">
-              <Col sm="4">
-                <FontAwesomeIcon icon={['fab', 'github']} className="icon" />
-                <h5>@jhipster-latino</h5>
-              </Col>
-              <Col sm="4">
-                <FontAwesomeIcon icon={['fab', 'twitter']} className="icon" />
-                <h5>@jhipster-latino</h5>
-              </Col>
-              <Col sm="4">
-                <FontAwesomeIcon icon={['fab', 'instagram']} className="icon" />
-                <h5>@jhipster_latino</h5>
-              </Col>
-            </Row>
-          </footer>
         </section>
       </div>
     );
--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,21 +1,26 @@
 import './footer.scss';
 
 import React from 'react';
-import { Translate } from 'react-jhipster';
 import { Col, Row } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = props => (
-  <div className="footer text-center strong">
-    <Row>
-      <Col md="12">
-        <em>
-          <p>
-            <Translate contentKey="footer">Your footer</Translate>
-          </p>
-        </em>
+  <footer className="container-fluid text-center bg-gray">
+    <Row className="text-center">
+      <Col sm="4">
+        <FontAwesomeIcon icon={['fab', 'github']} className="icon" />
+        <h5>@jhipster-latino</h5>
+      </Col>
+      <Col sm="4">
+        <FontAwesomeIcon icon={['fab', 'twitter']} className="icon" />
+        <h5>@jhipster-latino</h5>
+      </Col>
+      <Col sm="4">
+        <FontAwesomeIcon icon={['fab', 'instagram']} className="icon" />
+        <h5>@jhipster_latino</h5>
       </Col>
     </Row>
-  </div>
+  </footer>
 );
 
 export default Footer;
