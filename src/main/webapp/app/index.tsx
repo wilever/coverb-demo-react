585e4d870aee772fc5d9d5958d319e1fed563d6a
jhipster-latino <wilevergomez@gmail.com>
2019-01-30 11:06:40 -0300

    update home and readme

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -11,6 +11,7 @@ import setupAxiosInterceptors from './config/axios-interceptor';
 import { clearAuthentication } from './shared/reducers/authentication';
 import ErrorBoundary from './shared/error/error-boundary';
 import AppComponent from './app';
+import { loadIcons } from './config/icon-loader';
 
 const devTools = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
 
@@ -19,6 +20,8 @@ registerLocale(store);
 
 const actions = bindActionCreators({ clearAuthentication }, store.dispatch);
 setupAxiosInterceptors(() => actions.clearAuthentication('login.error.unauthorized'));
+
+loadIcons();
 
 const rootEl = document.getElementById('root');
 
